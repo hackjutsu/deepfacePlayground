24ef97cf8c8bc8d984d28d413d2cba01c9cc8287
CosmoX <cosmo.lepton@gmail.com>
2023-11-04 15:12:11 -0700

    moved the face recognition to background thread

--- face_recognition.py
+++ face_recognition.py
@@ -3,6 +3,7 @@ import os
 from deepface import DeepFace
 import pandas as pd
 import time
+import threading
 
 # Path to the known faces
 db_path = 'known_people/'
@@ -12,8 +13,51 @@ cap = cv2.VideoCapture(0)
 
 # Variables to keep track of time and labels
 last_recognition_time = time.time()
-label = 'detecting'
+label = 'detecting...'
 
+# Initialize the lock
+lock = threading.Lock()
+
+def recognize_face(face_image):
+    global label
+    try:
+        # Perform face recognition using DeepFace
+        search_result = DeepFace.find(img_path=face_area_rgb, db_path=db_path, enforce_detection=False, model_name='VGG-Face', distance_metric='cosine')
+
+        if len(search_result) > 0 and isinstance(search_result[0], pd.DataFrame) and not search_result[0].empty:
+            # Get the most similar face entry
+            best_match = search_result[0]
+            identity = best_match['identity']
+            similarity_score = best_match['VGG-Face_cosine']
+
+            if isinstance(identity, pd.Series):
+                identity = identity.iloc[0]  # Get the first item if it's a Series
+
+            person_name = os.path.dirname(identity).split(os.sep)[-1]
+
+            # Convert similarity_score to a single float value if it's a Series
+            if isinstance(similarity_score, pd.Series):
+                similarity_score = similarity_score.values[0]
+
+            if similarity_score < 0.20:
+                # Extract the person's name from the file path
+                person_name = os.path.basename(os.path.dirname(identity))
+                with lock:
+                    label = person_name
+                print(f"Best match: {person_name} with similarity score: {similarity_score}")
+            else:
+                with lock:
+                    label = "Unknown"
+                print("No similar faces found with high confidence.")
+        else:
+            with lock:
+                label = "Unknown"
+
+    except Exception as e:
+        print(f"An error occurred during recognition: {e}")
+
+
+recognition_thread = None
 while True:
     ret, frame = cap.read()  # Read the frame from the webcam
 
@@ -37,53 +81,24 @@ while True:
 
         # Perform face recognition every 5 seconds
         if time_elapsed > 5:
-            last_recognition_time = time.time()  # Reset the timer
-
             for (x, y, w, h) in faces:
                 cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
                 face_area = frame[y:y+h, x:x+w]
                 face_area_rgb = cv2.cvtColor(face_area, cv2.COLOR_BGR2RGB)
 
-                try:
-                    # Perform face recognition using DeepFace
-                    search_result = DeepFace.find(img_path=face_area_rgb, db_path=db_path, enforce_detection=False, model_name='VGG-Face', distance_metric='cosine')
-                    print(search_result)
+                # Start the recognition thread
+                recognition_thread = threading.Thread(target=recognize_face, args=(face_area_rgb,))
+                recognition_thread.start()
+                last_recognition_time = time.time() # Reset the timer
 
-                    if len(search_result) > 0 and isinstance(search_result[0], pd.DataFrame) and not search_result[0].empty:
-                        # Get the most similar face entry
-                        best_match = search_result[0]
-                        identity = best_match['identity']
-                        similarity_score = best_match['VGG-Face_cosine']
-
-                        if isinstance(identity, pd.Series):
-                            identity = identity.iloc[0]  # Get the first item if it's a Series
-
-                        person_name = os.path.splitext(os.path.basename(identity))[0]
-
-                        # Convert similarity_score to a single float value if it's a Series
-                        if isinstance(similarity_score, pd.Series):
-                            similarity_score = similarity_score.values[0]
-
-                        if similarity_score < 0.20:
-                            # Extract the person's name from the file path
-                            person_name = os.path.splitext(os.path.basename(identity))[0]
-                            label = person_name
-                            print(f"Best match: {person_name} with similarity score: {similarity_score}")
-                        else:
-                            label = "Unknown"
-                            print("No similar faces found with high confidence.")
-                    else:
-                        label = "Unknown"
-                        print("No faces recognized from DeepFace.find()")
-
-                except Exception as e:
-                    print(f"An error occurred during recognition: {e}")
+        with lock:
+            current_label = label
 
         # Draw rectangles and labels around each face
         for (x, y, w, h) in faces:
             cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
             # Put the label on the image
-            cv2.putText(frame, label, (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36, 255, 12), 2)
+            cv2.putText(frame, current_label, (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36, 255, 12), 2)
 
 
     # Display the resulting frame
@@ -96,3 +111,7 @@ while True:
 # Release the webcam and destroy all windows when done
 cap.release()
 cv2.destroyAllWindows()
+
+# Wait for the recognition thread to finish if it's running
+if recognition_thread is not None:
+    recognition_thread.join()
